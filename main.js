2e90ddfb1c31f33bc760e083fe69c8fe82d99066
aichangsub <aichangsub@gmail.com>
2026-01-10 10:28:31 +0000

    feat: Add i18n and dark/white mode

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-// main.js - This is where you can add interactivity to your site.
+// Dark mode toggle
+const themeSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        themeSwitch.checked = true;
+        document.body.classList.add('dark-mode');
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        document.body.classList.add('dark-mode');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        document.body.classList.remove('dark-mode');
+    }
+}
+
+if (themeSwitch) {
+    themeSwitch.addEventListener('change', switchTheme, false);
+}
